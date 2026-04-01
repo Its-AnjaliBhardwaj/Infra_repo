5045a55dd5de08bf58cfe43ab270018d628aa2f7
Its-AnjaliBhardwaj <59396669+Its-AnjaliBhardwaj@users.noreply.github.com>
2026-04-01 13:19:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 terraform {
   required_version = ">= 1.3"
 }
+
+module "gcp_basic" {
+  source = "git::https://github.com/Its-AnjaliBhardwaj/terraform-gcp-modules.git//gcp-basic?ref=main"
+
+  bucket_name = "infra-poc-bucket"
+  vpc_name    = "infra-poc-vpc"
+}
